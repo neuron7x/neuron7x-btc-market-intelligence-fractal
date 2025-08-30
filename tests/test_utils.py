b6ed368f334b3b5ebd7d2cc99989cd8165d93cad
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-08-30 21:52:47 +0300

    Use Real for numeric checks

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from fractions import Fraction
 
 import pytest
@@ -6,13 +5,15 @@ import pytest
 from btcmi.utils import is_number
 
 
-@pytest.mark.parametrize(
-    "val",
-    [1, 1.0, 1 + 2j, Decimal("1.23"), Fraction(1, 3)],
-)
-def test_is_number_with_numeric_types(val):
-    """``is_number`` should return True for standard numeric objects."""
+@pytest.mark.parametrize("val", [1, 1.0, Fraction(1, 3)])
+def test_is_number_with_real_numbers(val):
+    """``is_number`` should return True for real numeric objects."""
     assert is_number(val)
+
+@pytest.mark.parametrize("val", [1 + 2j, 2 + 0j])
+def test_is_number_rejects_complex_numbers(val):
+    """``is_number`` should return False for complex numbers."""
+    assert not is_number(val)
 
 
 @pytest.mark.parametrize("val", [True, False, "1", None, [], object()])
